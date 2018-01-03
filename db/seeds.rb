a183db070b6d6017a24d640c7cab9d688c24e95b
supritkumar shah <suprit7@gmail.com>
2018-01-02 20:58:38 -0500

    wors with dummy data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+u1 = User.create(email: 'user@example.com', password:'password')
+u2 = User.create(email: 'user2@example.com', password: 'password')
+
+p1 = u1.posts.create(title: 'First post', body:'An Airplane')
+p2 = u2.posts.create(title: 'second post', body:'A Train')
+
+p3 = u1.posts.create(title: 'Third post', body:'An Truck')
+p4 = u2.posts.create(title: 'Fourth post', body:'A Boat')
+
+p3.comments.create(body: "This post was terrible", user: u1)
+p4.comments.create(body: "This post was best thing in the whole world", user: u1)
